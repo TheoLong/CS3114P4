da162b65f4c0c2b0a5c9c47afb3ee79771d5d9e6
theoloong <theoloong@gmail.com>
2016-11-21 19:32:57 -0500

    more graph tests

--- F16P4starter/src/GraphTest.java
+++ F16P4starter/src/GraphTest.java
@@ -18,18 +18,18 @@ public class GraphTest extends TestCase
      */
     public void setUp()
     {
-        g = new Graph(5);
+        g = new Graph(6);
     }
     /**
      * test insertion
      */
     public void testInsertion()
     {
-        int numOfHandles = 9;
+        int numOfHandles = 100;
         ArrayList<Handle> handles = new ArrayList<Handle>();
         for (int i = 0; i < numOfHandles; i++)
         {
-            assertEquals(true , handles.add(new Handle(i)));
+            assertEquals(true , handles.add(new Handle(i*2+1)));
             g.insertVertex(new Handle(i));
         }
         
@@ -48,11 +48,11 @@ public class GraphTest extends TestCase
      */
     public void testAddEdge()
     {
-        int numOfHandles = 9;
+        int numOfHandles = 100;
         ArrayList<Handle> handles = new ArrayList<Handle>();
         for (int i = 0; i < numOfHandles; i++)
         {
-            assertEquals(true , handles.add(new Handle(i)));
+            assertEquals(true , handles.add(new Handle(i*2+1)));
             g.insertVertex(new Handle(i));
         }
         
@@ -103,16 +103,10 @@ public class GraphTest extends TestCase
         ArrayList<Handle> handles = new ArrayList<Handle>();
         for (int i = 0; i < numOfHandles; i++)
         {
-            assertEquals(handles.add(new Handle(i)), true);
-            g.insertVertex(new Handle(i));
+            assertEquals(handles.add(new Handle(i*2+1)), true);
+            g.insertVertex(new Handle(i*2+1));
         }
         
-        g.delete(new Handle(0));
-        
-        assertEquals(g.addEdge(handles.get(1), handles.get(2)), true);
-        assertEquals(g.addEdge(handles.get(1), handles.get(3)), true);
-        assertEquals(g.addEdge(handles.get(1), handles.get(4)), true);
-        assertEquals(g.addEdge(handles.get(1), handles.get(5)), true);
         g.printList();
         g.printGraph();
         for (int i = 1; i < numOfHandles; i++)
@@ -120,5 +114,18 @@ public class GraphTest extends TestCase
             g.delete(handles.get(i));
         }
         
+    }
+    public void testProbing()
+    {
+        g.insertVertex(new Handle(6));
+        g.insertVertex(new Handle(12));
+        g.insertVertex(new Handle(18));
+        g.delete(new Handle(6));
+        g.delete(new Handle(12));
+        g.delete(new Handle(18));
+        //reuse tombstone
+        g.insertVertex(new Handle(6));
+        g.insertVertex(new Handle(12));
+        g.insertVertex(new Handle(4));
     }
 }
